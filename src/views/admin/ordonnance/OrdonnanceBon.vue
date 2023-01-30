609a3aab18e9e2e5e990dedff1b03f446fa2a590
sedjronicaise <sedjronicaise@gmail.com>
2023-01-30 14:49:30 +0100

    praticiens updating

--- src/views/admin/ordonnance/OrdonnanceBon.vue
+++ src/views/admin/ordonnance/OrdonnanceBon.vue
@@ -5,39 +5,37 @@
             <div class="container">
                 <div class="row">
                     <div class="col-lg-10 text-center">
-                        <div class="row mt-3 text-align-center">
+                        <div class="row mt-3 align-self-end">
                             <div class="col-lg-6">
                                 <router-link to="/admin/ordonnance/index">
-                                    <div class="card pull-up">
-                                        <div class="card-content">
-                                            <div class="card-body">
-                                                <div class="media d-flex">
-                                                    <div class="align-self-center">
-                                                        <i class="la la-calendar-check-o font-large-2 info" style="font-size:12rem;"></i>
-                                                    </div>
-                                                    <div class="media-body text-right">
-                                                        <h5 class="text-muted text-bold-500">ORDONNANCE</h5>
-                                                        <h3 class="text-bold-600">15</h3>
-                                                    </div>
+                                    <div class="card pull-up" style="height: 300px; width: 400px;">
+
+                                        <div class="card" >
+                                            <div class="card-header">
+                                                <h4 class="text-center"><i class="la la-calendar-check-o font-large-2 info" style="font-size:12rem;"></i> ORDONNANCE</h4>
+                                            </div>
+                                            <div class="card-content">
+                                                <div class="card-body text-center">
+                                                    <p>Gestion des ordonnances.</p>
+                                                    <div class="badge badge-secondary">15</div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </div>                              
                                     </div>
                                 </router-link>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-6 ">
                                 <router-link to="/admin/bon/index">
-                                    <div class="card pull-up">
-                                        <div class="card-content">
-                                            <div class="card-body">
-                                                <div class="media d-flex">
-                                                    <div class="align-self-center">
-                                                        <i class="la la-calendar-check-o font-large-2 info" style="font-size:12rem;"></i>
-                                                    </div>
-                                                    <div class="media-body text-right">
-                                                        <h5 class="text-muted text-bold-500">BON D'EXAMENS</h5>
-                                                        <h3 class="text-bold-600">20</h3>
-                                                    </div>
+                                    <div class="card pull-up" style="height: 300px; width: 400px;">
+                                        <div class="card" >
+                                            <div class="card-header">
+                                                <h4 class="text-center">
+                                                    <i class="la la-edit font-large-2 info" style="font-size:12rem;"></i> BONS D'EXAMENS</h4>
+                                            </div>
+                                            <div class="card-content">
+                                                <div class="card-body text-center">
+                                                    <p>Gestion des bons d'examens.</p>
+                                                    <div class="badge badge-secondary">15</div>
                                                 </div>
                                             </div>
                                         </div>
